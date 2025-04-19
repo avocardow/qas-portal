f1265f1ce9a328b107506b9d4988447993927b62
Rowan Cardow <rowan@cardow.co>
2025-04-19 11:43:50 +1000

    feat(protected): move protected AppLayout into protected pages folder

--- src/app/(pages)/protected/layout.tsx
+++ src/app/(pages)/protected/layout.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
-import AppLayout from "@/app/(app)/layout";
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "@/server/auth";
+import AdminLayout from "@/app/(admin)/layout";
 
-export default async function ProtectedLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <AppLayout>{children}</AppLayout>;
+export default async function AppLayout({ children }: { children: ReactNode }) {
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/signin");
+  }
+  return <AdminLayout>{children}</AdminLayout>;
 }
