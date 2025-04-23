8b66365097b4ed2728e98cfdf883e52ebe6d7063
Rowan Cardow <rowan@cardow.co>
2025-04-23 18:31:50 +1000

    fix(4.4): Make ChatListProps optional with default values

--- src/components/chats/ChatList.tsx
+++ src/components/chats/ChatList.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 interface ChatListProps {
-  isOpen: boolean;
-  onToggle: () => void;
+  isOpen?: boolean;
+  onToggle?: () => void;
 }
 
-export default function ChatList({ isOpen, onToggle }: ChatListProps) {
+export default function ChatList({
+  isOpen = true,
+  onToggle = () => {},
+}: ChatListProps) {
   return (
     <div className={`p-4 ${isOpen ? "block" : "hidden"} xl:block`}>
       <button className="mb-4 xl:hidden" onClick={onToggle}>
