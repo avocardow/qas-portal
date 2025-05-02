dd039679412efac43a996b1669ae8c5f2197d4f5
Rowan Cardow <rowan@cardow.co>
2025-05-02 18:02:51 +1000

    feat(client): add placeholder overview and details components to Client Profile page\n\n- Created ClientDetailsSection component\n- Imported and rendered ClientOverviewCard and ClientDetailsSection in page.tsx

--- src/app/(pages)/(dashboard)/(app)/clients/[clientId]/page.tsx
+++ src/app/(pages)/(dashboard)/(app)/clients/[clientId]/page.tsx
@@ -1,6 +1,8 @@
 // Removed placeholder stub and DashboardPlaceholderPageTemplate
 
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import ClientOverviewCard from '@/components/clients/ClientOverviewCard';
+import ClientDetailsSection from '@/components/clients/ClientDetailsSection';
 
 export default function ClientDetailPage() {
   return (
@@ -8,10 +10,10 @@ export default function ClientDetailPage() {
       <PageBreadcrumb pageTitle="Client Profile" />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 xl:col-span-4">
-          {/* TODO: Client Overview Card */}
+          <ClientOverviewCard />
         </div>
         <div className="col-span-12 xl:col-span-8">
-          {/* TODO: Client Details Section */}
+          <ClientDetailsSection />
         </div>
       </div>
     </div>
---
+++ src/components/clients/ClientDetailsSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ComponentCard from '@/components/common/ComponentCard';
+
+export default function ClientDetailsSection() {
+  return (
+    <ComponentCard title="Client Details">
+      <p>This section will display detailed client information.</p>
+    </ComponentCard>
+  );
+} 
